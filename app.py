27f62dcee72c4c84678c1959bb0820834d76cb41
Rickson Ramon Pereira Pinto <20211041110026@ifrn.local>
2024-03-13 08:07:17 -0300

    adjakdj

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+#rota para a questao 1
+
+#rota para a questão 2
+
+#rota para a questão 3
+
+#rota para a questão 4
+
+#rota para a questão 5
+
+if __name__== '__main__':
+  app.run()
